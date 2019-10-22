e5a21be8714a134836961d46a0536fb464286ef1
Martin Braquet <martin.braquet@student.uclouvain.be>
2019-10-22 23:02:22 +0200

    Add heuristics

--- assignment2/pacmen.py
+++ assignment2/pacmen.py
@@ -5,6 +5,7 @@
 '''
 import time
 from search import *
+import numpy as np
 
 
 #################
@@ -52,8 +53,9 @@ class Pacmen(Problem):
             return newgrid
         
         for k in range(len(out_list)):
-            if len(out_list[k]) != len(set(out_list[k])):
+            if len(out_list[k]) != len(set(out_list[k])):   # Remove new states where 2 pacmen are on the same tile
                 continue
+            out_food_list = state.food_list.copy()
             newgrid = my_copy_list(state)
             nfoods = state.nfoods
             for (i,j) in state.pac_list:   # Remove the previous pacmen
@@ -61,8 +63,9 @@ class Pacmen(Problem):
             for (i,j) in out_list[k]:      # Add the new pacmen
                 if newgrid[i][j] == '@':
                     nfoods -= 1
+                    out_food_list.remove((i,j)) 
                 newgrid[i][j] = '$'
-            newstate = State(newgrid, out_list[k], npacs=state.npacs, nfoods=nfoods)  # New state
+            newstate = State(grid=newgrid, pac_list=out_list[k], food_list=out_food_list, npacs=state.npacs, nfoods=nfoods)  # New state
             yield(0, newstate)             # Yield the action (0 because the paths are costless) and the state to the 'expand' method
 
 
@@ -94,7 +97,15 @@ class State:
         self.food_list = food_list
         self.npacs = npacs
         self.nfoods = nfoods
-                        
+
+        self.pac_array = np.zeros((npacs,2))
+        for i in range(npacs):
+                 self.pac_array[i][0] = pac_list[i][0]
+                 self.pac_array[i][1] = pac_list[i][1]
+        self.food_array = np.zeros((nfoods,2))
+        for i in range(nfoods):
+                 self.food_array[i][0] = food_list[i][0]
+                 self.food_array[i][1] = food_list[i][1]
             
 
     def __str__(self):
@@ -146,8 +157,34 @@ def readInstanceFile(filename):
 # Heuristic function #
 ######################
 def heuristic(node):
-    h = 0
-    return h
+    state = node.state
+    if state.nfoods == 0:
+        return 0
+    food_array = np.copy(state.food_array)
+    pac_array = np.copy(state.pac_array)
+    food_array_3D = np.dstack([food_array]*state.npacs)
+    pac_array_3D = np.dstack([pac_array]*state.nfoods)
+    pac_array_3D = np.transpose(pac_array_3D, (2, 1, 0))
+    h = np.zeros((state.npacs,))
+    for k in range(state.nfoods):
+        dist = np.abs(food_array_3D - pac_array_3D)
+        dist = np.sum(dist, axis=1)           # Distance of each Pacman compared to each food
+        (i,j) = np.unravel_index(dist.argmin(), dist.shape)
+        h[j] += dist[i][j]
+        pac_array[j] = food_array[i]
+        food_array_3D = np.delete(food_array_3D, i, 0)
+        food_array = np.delete(food_array, i, 0)
+        '''
+        if ((k+1) % state.npacs == 0):
+            pac_array_3D = np.dstack([np.copy(state.pac_array)]*(state.nfoods-(k+1))
+        else:
+            pac_array_3D = np.delete(pac_array_3D, i, 0)'''
+        if (k == state.nfoods - 1):
+            break
+        pac_array_3D = np.dstack([pac_array]*(state.nfoods-k-1))
+        pac_array_3D = np.transpose(pac_array_3D, (2, 1, 0))
+        
+    return max(h)
 
 
 #####################
@@ -155,7 +192,6 @@ def heuristic(node):
 #####################
 grid_init = readInstanceFile(sys.argv[1])
 init_state = State(grid_init)
-print(init_state)
 
 problem = Pacmen(init_state)
 
