23f68b1d1750611f722b7f205bb1a8363bc1d25f
Gilles Peiffer <gilles.peiffer@outlook.com>
2019-10-23 16:37:53 +0200

    finalize code + update experiments

--- assignment2/pacmen.py
+++ assignment2/pacmen.py
@@ -5,7 +5,6 @@
 '''
 import time
 from search import *
-import numpy as np
 
 
 #################
@@ -14,100 +13,74 @@ import numpy as np
 class Pacmen(Problem):
 
     def successor(self, state):
-        ll = []
-        for k in range(state.npacs):
-            l = []
-            (i, j) = state.pac_list[k]
-            l.append((i,j))
-            if i > 0 and state.grid[i-1][j] != 'x':
-                l.append((i-1,j))
-            if (i < state.nbr - 1) and state.grid[i+1][j] != 'x':
-                l.append((i+1,j))
-            if j > 0 and state.grid[i][j-1] != 'x':
-                l.append((i,j-1))
-            if (j < state.nbc - 1) and state.grid[i][j+1] != 'x':
-                l.append((i,j+1))
-            ll.append(l)
+        states = [("t", state)]
+        # Iterating over the pacmen
         
-        def rec(ll, new_list, out_list, k):
-            if k == len(ll):
-                out_list.append(new_list.copy())
-                new_list.pop(-1)
-                return
-            for i in range(len(ll[k])):
-                new_list.append(ll[k][i])
-                rec(ll, new_list, out_list, k+1)
-            if k != 0:
-                new_list.pop(-1)
-                
-        out_list = []
-        rec(ll, [], out_list, 0)
-        out_list.pop(0)
-        
-        def my_copy_list(state):
-            newgrid = []
+        def findpac(state):
+            pac_list = []
             for i in range(state.nbr):
-                newgrid.append([" "] * state.nbc)
                 for j in range(state.nbc):
-                    newgrid[i][j] = state.grid[i][j]
-            return newgrid
+                    if state.grid[i][j] == '$':
+                        pac_list.append((i,j))
+            return pac_list
         
-        for k in range(len(out_list)):
-            if len(out_list[k]) != len(set(out_list[k])):   # Remove new states where 2 pacmen are on the same tile
-                continue
-            out_food_list = state.food_list.copy()
-            newgrid = my_copy_list(state)
-            nfoods = state.nfoods
-            for (i,j) in state.pac_list:   # Remove the previous pacmen
-                newgrid[i][j] = ' '
-            for (i,j) in out_list[k]:      # Add the new pacmen
-                if newgrid[i][j] == '@':
-                    nfoods -= 1
-                    out_food_list.remove((i,j)) 
-                newgrid[i][j] = '$'
-            newstate = State(grid=newgrid, pac_list=out_list[k], food_list=out_food_list, npacs=state.npacs, nfoods=nfoods)  # New state
-            yield(0, newstate)             # Yield the action (0 because the paths are costless) and the state to the 'expand' method
-
+        pac_list = findpac(state)
+        
+        for pacman in pac_list:
+            possibilities = []
+            # For each pacman, we iterate other the list of states left by the previous pacman, this list is only composed of the initial state for the first pacman
+            for s in states:
+                # This helps define the different directions in which the pacmen can move, they always move, because not moving is rarely optimal
+                for move in [-1, 1]:
+                    # Checking if the next positions are admissible
+                    if 0 <= pacman[0] + move < s[1].nbr and s[1].grid[pacman[0]+move][pacman[1]] != "x" and s[1].grid[pacman[0]+move][pacman[1]] != "$":
+                        newgrid = [row[:] for row in s[1].grid]
+                        newgrid[pacman[0]][pacman[1]] = " "
+                        newgrid[pacman[0]+move][pacman[1]] = "$"
+                        possibilities.append(("t", State(newgrid)))
+                    if 0 <= pacman[1] + move < s[1].nbc and s[1].grid[pacman[0]][pacman[1]+move] != "x" and s[1].grid[pacman[0]][pacman[1]+move] != "$":
+                        newgrid = [row[:] for row in s[1].grid]
+                        newgrid[pacman[0]][pacman[1]] = " "
+                        newgrid[pacman[0]][pacman[1]+move] = "$"
+                        possibilities.append(("t", State(newgrid)))
+            states = possibilities[:] # The list of states from which pacmen will add their moves is updated to the list of states with all the states containing the different combinations of moves of the previous pacmen
+        return states
 
     def goal_test(self, state):
-        return state.nfoods == 0
+        for i in range(state.nbr):
+            for j in range(state.nbc):
+                if state.grid[i][j] == "@":
+                    return False
+        return True
 
 
 ###############
 # State class #
 ###############
 class State:
-    def __init__(self, grid, pac_list=None, food_list=None, npacs=None, nfoods=None):
+    def __init__(self, grid):
         self.nbr = len(grid)
         self.nbc = len(grid[0])
         self.grid = grid
+        """
         if pac_list == None:
             pac_list = []
-            food_list = []
-            nfoods = npacs = 0
             for i in range(self.nbr):
                 for j in range(self.nbc):
                     if grid[i][j] == '$':
                         pac_list.append((i,j))
-                        npacs += 1
-                    elif grid[i][j] == '@':
-                        food_list.append((i,j))
-                        nfoods += 1
+        if food_list == None:
+            food_list = []
+            for i in range(self.nbr):
+                for j in range(self.nbc):
+                    if grid[i][j] == '@':
+                        food_list.append((i,j))     
         self.pac_list = pac_list
         self.food_list = food_list
-        self.npacs = npacs
-        self.nfoods = nfoods
-
-        self.pac_array = np.zeros((npacs,2))
-        for i in range(npacs):
-                 self.pac_array[i][0] = pac_list[i][0]
-                 self.pac_array[i][1] = pac_list[i][1]
-        self.food_array = np.zeros((nfoods,2))
-        for i in range(nfoods):
-                 self.food_array[i][0] = food_list[i][0]
-                 self.food_array[i][1] = food_list[i][1]
-            
-
+        self.npacs = len(pac_list)
+        self.nfoods = len(food_list)
+        """
+        
     def __str__(self):
         nsharp = self.nbc * 2 + 3
         s = "#" * nsharp
@@ -121,23 +94,13 @@ class State:
                 s += '\n'
         s += "#" * nsharp
         return s
-
+    
+    # Two grids are equal if their elements are the same
     def __eq__(self, other_state):
-        for i in range(self.nbr):                                            # Compare each tile one-by-one
-            for j in range(self.nbc):
-                if self.grid[i][j] != other_state.grid[i][j]:
-                    return False
-        return True
-
+        return self.grid == other_state.grid
+    # Same logic as equal, the idea is that two states with similar grids will have the same hash.
     def __hash__(self):
-        ctr = 0
-        for i in range(self.nbr):
-            for j in range(self.nbc):
-                if self.grid[i][j] == "$":
-                    ctr += (self.nbr * i + j) * 4 + 1
-                elif self.grid[i][j] == "@":
-                    ctr += (self.nbr * i + j) * 4 + 2
-        return ctr
+        return hash(str(self.grid))
 
 
 
@@ -156,39 +119,80 @@ def readInstanceFile(filename):
 ######################
 # Heuristic function #
 ######################
+"""
 def heuristic(node):
-    state = node.state
-    if state.nfoods == 0:
-        return 0
-    food_array = np.copy(state.food_array)
-    pac_array = np.copy(state.pac_array)
-    food_array_3D = np.dstack([food_array]*state.npacs)
-    pac_array_3D = np.dstack([pac_array]*state.nfoods)
-    pac_array_3D = np.transpose(pac_array_3D, (2, 1, 0))
-    h = np.zeros((state.npacs,))
-    for k in range(state.nfoods):
-        dist = np.abs(food_array_3D - pac_array_3D)
-        dist = np.sum(dist, axis=1)           # Distance of each Pacman compared to each food
-        (i,j) = np.unravel_index(dist.argmin(), dist.shape)
-        h[j] += dist[i][j]
-        pac_array[j] = food_array[i]
-        food_array_3D = np.delete(food_array_3D, i, 0)
-        food_array = np.delete(food_array, i, 0)
-        '''
-        if ((k+1) % state.npacs == 0):
-            pac_array_3D = np.dstack([np.copy(state.pac_array)]*(state.nfoods-(k+1)))
-        else:
-            pac_array_3D = np.delete(pac_array_3D, i, 0)'''
-        if (k == state.nfoods - 1):
-            break
-        pac_array_3D = np.dstack([pac_array]*(state.nfoods-k-1))
-        pac_array_3D = np.transpose(pac_array_3D, (2, 1, 0))
+    def manhattan(pos1, pos2):
+        return abs(pos1[0]-pos2[0])+abs(pos1[1]-pos2[1])
+    
+    (foods, pacmen) = node.state.food_list.copy(), node.state.pac_list.copy()
+    
+    l = [0]*len(pacmen)
+    
+    def closest_food(pac):
+        tmp = manhattan(pac,foods[0])
+        closest = tmp
+        best = foods[0]
+        for f in foods[1:]:
+            candidate = manhattan(pac,f)
+            if candidate < closest:
+                closest = candidate
+                best = f
+        return best, closest
+    
+    def closest_pair():
+        minimum = [0]*len(pacmen)
+        bestfood = [foods[0]]*len(pacmen)
+        for i in range(len(pacmen)):
+            pac = pacmen[i]
+            bestfood[i], minimum[i] = closest_food(pac)
         
-    return max(h)
+        besti = minimum.index(min(minimum))
+        return bestfood[besti], minimum[besti], besti
+            
+    while foods:
+        b, m, i = closest_pair()
+        l[i] += m
+        foods.remove(b)
+            
+    return sum(l)/len(l)
+"""
+
+def heur(node):
+    def manhattan(pos1, pos2):
+        return abs(pos1[0]-pos2[0])+abs(pos1[1]-pos2[1])
+    
+    def findpf(state):
+            pac_list = []
+            food_list = []
+            for i in range(state.nbr):
+                for j in range(state.nbc):
+                    if state.grid[i][j] == '$':
+                        pac_list.append((i,j))
+                    elif state.grid[i][j] == '@':
+                        food_list.append((i,j))
+            return food_list, pac_list
+    
+    (foods, pacmen) = findpf(node.state)
+    
+    l = [0]*len(foods)
+    
+    def closest_pac(food):
+        tmp = manhattan(food,pacmen[0])
+        closest = tmp
+        for pac in pacmen[1:]:
+            candidate = manhattan(pac,food)
+            if candidate < closest:
+                closest = candidate
+        return closest
+    
+    for i in range(len(foods)):
+        l[i] = closest_pac(foods[i])
+    
+    return max(l, default=0)
 
 def zero(node):
     return 0
-
+        
 
 #####################
 # Launch the search #
@@ -200,8 +204,8 @@ init_state = State(grid_init)
 problem = Pacmen(init_state)
 
 startTime = time.perf_counter()
-node, nbExploredNodes = astar_graph_search(problem,heuristic)
-#node, nbExploredNodes = best_first_graph_search(problem)
+node, nbExploredNodes = astar_graph_search(problem,heur)
+#node, nbExploredNodes = breadth_first_graph_search(problem)
 endTime = time.perf_counter()
 
 # example of print
